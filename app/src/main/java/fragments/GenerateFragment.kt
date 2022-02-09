6827bd6c0500a030eb9f94c40666f90edc6ef4b5
Mantu Kumar <mk420464@gmail.com>
2022-02-09 20:57:41 +0530

    base QRather version 1

--- app/src/main/java/fragments/GenerateFragment.kt
+++ app/src/main/java/fragments/GenerateFragment.kt
@@ -1,15 +1,23 @@
 package fragments
 
+import android.graphics.Bitmap
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.qrather.R
 import com.example.qrather.databinding.FragmentGenerateBinding
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.WriterException
+import com.google.zxing.qrcode.QRCodeWriter
+
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -27,6 +35,9 @@ class GenerateFragment : Fragment() {
     private var param2: String? = null
 
     private lateinit var binding: FragmentGenerateBinding
+    private lateinit var qrcode : ImageView
+    private lateinit var txt_content : EditText
+    private lateinit var btn_generate : Button
 
 
 
@@ -54,10 +65,41 @@ class GenerateFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding = FragmentGenerateBinding.inflate(layoutInflater)
+
         val btn_generate = view.findViewById<Button>(R.id.btn_generate)
 
+        qrcode = view.findViewById(R.id.qrcode)
+        txt_content = view.findViewById(R.id.txt_content)
+
+
         btn_generate.setOnClickListener {
-            Toast.makeText(requireActivity(), "working", Toast.LENGTH_LONG).show()
+
+
+            val data = txt_content.text.toString().trim()
+
+            if (data.isEmpty()){
+                Toast.makeText(requireActivity(),"Field is Empty",Toast.LENGTH_SHORT).show()
+            }else{
+                val writer = QRCodeWriter()
+                try {
+                    val bitMatrix = writer.encode(data, BarcodeFormat.QR_CODE,512,512)
+                    val width = bitMatrix.width
+                    val height = bitMatrix.height
+                    val bmp  = Bitmap.createBitmap(width,height,Bitmap.Config.RGB_565)
+                    for(x in 0  until width){
+                        for(y in 0 until height){
+                            bmp.setPixel(x,y, if(bitMatrix[x,y]) Color.BLACK else Color.WHITE)
+                        }
+                    }
+                    qrcode.setImageBitmap(bmp)
+
+                }catch (e:WriterException){
+                    e.printStackTrace()
+                }
+            }
+
         }
 
 
